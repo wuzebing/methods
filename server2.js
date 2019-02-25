32c8c156a9b22a173197bcd013609acfb37b1a04
“wuzebing5；;git config --global user.email “qinaide000 <wuzebing@guazi.com>
2019-02-25 15:15:19 +0800

    update

--- server2.js
+++ server2.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-app.get("/api/test", function(req, res) {
-  res.send('success');
+app.get("/jsonp/test", function(req, res) {
+  res.send('callback(1111)');
 })
 
-app.listen(8189, function(){
+app.listen(8188, function(){
   console.log("Server running at %s", 8189)
 });
